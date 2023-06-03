b23ab78afb86e1e413f0dae18d24f7833dcd8f4a
vishal-codes <itsvishal2417@gmail.com>
2023-06-03 15:56:50 +0530

    feat: backend file structure

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const uuid4 = require('uuid4');
 
@@ -8,16 +9,7 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-Requested-With, Content-Type, Authorization'
-    );
-    next();
-});
-app.use(express.json({ limit: '10MB' }));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello, welocme to Seatopia's API");
@@ -34,7 +26,7 @@ mongoose
 
 app.listen(PORT, () =>
     console.log(
-        "Hello! This is comfort space's backend, listening on port - ",
+        "Hello! This is Seatopia's backend, listening on port - ",
         PORT
     )
 );
