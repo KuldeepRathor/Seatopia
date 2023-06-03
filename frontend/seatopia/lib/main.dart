44a757eff78044e914b4c6aaceb5cd8980cf4037
Kuldeep Rathor <90392158+KuldeepRathor@users.noreply.github.com>
2023-06-03 16:46:04 +0530

    login signup

--- frontend/seatopia/lib/main.dart
+++ frontend/seatopia/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:seatopia/screens/auth/signup_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,16 +8,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Seatopia',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Seatopia'),
-        ),
-        body: const Center(
-          child: Text('Seatopia'),
-        ),
-      ),
-    );
+    return MaterialApp(title: 'Seatopia', home: BarberSignUpPage());
   }
 }
---
+++ frontend/seatopia/lib/routes.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class Routes {
+  static const String initalRoute = "/";
+
+  // Barber Routes
+  static const String barberLoginRoute = "/barber/login";
+  static const String barberSignUpRoute = "/barber/signup";
+  static const String barberHomeRoute = "/barber/home";
+
+  // Customer Routes
+  static const String customerLoginRoute = "/customer/login";
+  static const String customerSignUpRoute = "/customer/signup";
+  static const String customerHomeRoute = "/customer/home";
+
+  // Generatl Routes
+  static const String mapRoute = "/mapPage";
+  static const String selectTimeRoute = "/select-time";
+  static const String availableShops = "/available-shops";
+
+  // static Route<dynamic> getRoutes(RouteSettings settings) {
+  //   final args = settings.arguments;
+
+  // switch (settings.name) {
+  //   case Routes.initalRoute:
+  //     return getRoute(const WelcomePage());
+  //   // Barber
+  //   case Routes.barberLoginRoute:
+  //     return getRoute(const BarberLoginPage());
+  //   case Routes.barberSignUpRoute:
+  //     return getRoute(const BarberSignUpPage());
+  //   case Routes.barberHomeRoute:
+  //     return getRoute(const BarberHomePage());
+
+  //   // Customer
+  //   case Routes.customerLoginRoute:
+  //     return getRoute(const CustomerLoginPage());
+  //   case Routes.customerSignUpRoute:
+  //     return getRoute(const CustomerSignUpPage());
+  //   case Routes.customerHomeRoute:
+  //     return getRoute(const CustomerHomePage());
+
+  //   // General Routes
+  //   case Routes.mapRoute:
+  //     return getRoute(const MapsPage());
+  //   case Routes.selectTimeRoute:
+  //     return getRoute(const SelectTimePage());
+  //   case Routes.availableShops:
+  //     return getRoute(AvailableShopsPage(time: args as String));
+
+  //   default:
+  //     return getRoute(const WelcomePage());
+  // }
+}
+
+  // static MaterialPageRoute getRoute(Widget route) {
+  //   return MaterialPageRoute(builder: (_) => route);
+  // }
+// }
